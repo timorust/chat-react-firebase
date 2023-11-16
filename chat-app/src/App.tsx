a46245b1fe126f6630d7732e49e724dff1002a3e
tamir.rust <timorust1@gmail.com>
2023-11-16 12:08:16 +0200

    react query

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
 import "./App.css";
+import { getDocs, collection } from "firebase/firestore";
+import { firebaseDB } from "./firebase_connection";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
+  async function readData() {
+    const collectionTest = collection(firebaseDB, "test");
+    const querySet = await getDocs(collectionTest);
+    for (const doc of querySet.docs) {
+      console.log(doc.data());
+    }
+  }
+  readData();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Timorust</h1>
-    </>
+    </QueryClientProvider>
   );
 }
 
